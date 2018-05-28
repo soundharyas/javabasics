095ca41b4e86be25e9689f893e55b5290aa8f3a4
soundharyas <soundharyasss98@gmail.com>
2018-05-29 05:51:36 +1100

    basic

---
+++ src/com/basic/DataTypeExample.java
@@ -0,0 +1,10 @@
+package com.basic;
+
+public class DataTypeExample {
+	public static void main (String s[]) {
+	byte one = 80;
+	byte two = 30;
+	int three =  one + two;
+	System.out.println(three);
+	}
+}
---
+++ src/com/basic/OperatorExample.java
@@ -0,0 +1,24 @@
+package com.basic;
+
+public class OperatorExample {
+    public static void main(String [] arg)
+    
+    {
+    	int count =10;
+    	count++;
+    	count--;
+    	int negcount =-20;
+    	boolean program = false;
+    	System.out.println(!program);
+    	System.out.println(count < 20);
+    	System.out.println(count > 20);
+    	
+    	System.out.println(count >= 10);
+    	System.out.println(count <= 10);
+    	
+    	
+    	System.out.println(7 & 2);
+    	System.out.println(9 | 10);
+    
+    }
+}
