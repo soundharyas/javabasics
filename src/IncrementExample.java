99029ee75e25c65c6f553c24ab5d97c1dd571433
soundharyas <soundharyasss98@gmail.com>
2018-05-30 15:25:28 +1100

    age.java

---
+++ src/IncrementExample.java
@@ -0,0 +1,12 @@
+
+public class IncrementExample {
+	public static void main(String []args)
+	{
+		int age=65;
+		if(age>= 65);
+		System.out.print.ln("Age is greater than or equal to 65");
+		else
+		System.out.println("Age is lessthan 65");
+	}
+
+}
---
+++ src/com/basic/age.java
@@ -0,0 +1,13 @@
+package com.basic;
+
+public class age {
+	public static void main(String []args) {
+		int age = 65;
+	if ( age >= 65 )
+    System.out.println( "Age is greater than or equal to 65" );
+    else
+    System.out.println( "Age is less than 65 ");
+
+	}
+
+}
