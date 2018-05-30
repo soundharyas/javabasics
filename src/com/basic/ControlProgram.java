45d8d67f625f3bb5e43d6e26749535fdf63eec4e
soundharyas <soundharyasss98@gmail.com>
2018-05-30 15:46:40 +1100

    ControlProgram

---
+++ src/com/basic/ControlProgram.java
@@ -0,0 +1,14 @@
+package com.basic;
+
+public class ControlProgram {
+public static void main(String []args)
+{
+	int count = 10;
+	if (count < 20)
+		System.out.println(10);
+	else
+		if(count == 20)
+	System.out.println(20);
+	
+} 
+}
