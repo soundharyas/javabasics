9b6e15a3e32e1ef627001c1715c301f6eee2c93c
soundharyas <soundharyasss98@gmail.com>
2018-05-30 18:06:12 +1100

    Loops.java

---
+++ src/com/basic/Loops.java
@@ -0,0 +1,21 @@
+package com.basic;
+
+public class Loops {
+	public static void main(String []args) {
+		int i=0;
+		for(i=0;i<10;i++)
+			System.out.println("for loop i =" +i);
+	     i++;
+	
+	int i=0;
+     do {
+	 System.out.println("do loop i =" +i);
+		 ++i;
+     } while(i < 10);
+ }
+	}
+
+	
+	
+	
+
