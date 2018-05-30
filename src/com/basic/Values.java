578e6125c42ce99bade21f2bf0b796eda60ffada
soundharyas <soundharyasss98@gmail.com>
2018-05-30 15:56:17 +1100

    Values

---
+++ src/com/basic/Values.java
@@ -0,0 +1,18 @@
+package com.basic;
+
+public class Values {
+	public static void main(String []args) {
+		
+	
+	int x = 1, total;
+	while ( x <= 10 )
+	{
+	total  = x;
+	System.out.println(x);
+	++x;
+	}
+	}
+	}
+	
+
+
